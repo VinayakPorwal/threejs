ac9be7e53f686190e1511429b26eb21a85375126
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2022-02-16 13:48:44 +0530

    Updates

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import * as THREE from "https://unpkg.com/three@0.120.1/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.120.1/examples/jsm/controls/OrbitControls.js";
-// import { searchForWorkspaceRoot } from "vite";
-// import logo from "./earth.jpg";
-// import logo2 from "./logo2.png";
+// import {TimeLineMax} from "https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js";
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -14,90 +14,145 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById("main"),
 });
-// document.body.appendChild(renderer.domElement);
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xffff, 0.5);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// camera.position.setZ(40);
+camera.position.setY(10);
 
-camera.position.setZ(30);
 
-const crystalGeometry = new THREE.SphereGeometry(10,100,100);
-const crystalGeometry2 = new THREE.CubeGeometry(10,10,10);
-const texture = new THREE.TextureLoader().load('./earth.jpg');
-const texture4 = new THREE.TextureLoader().load('./logo2.png');
-// const shinyMaterial = new THREE.MeshStandardMaterial({
-//   color: 0x4086f4,
-//   metalness: 0.1,
-//   roughness: 0.2,
-//   shininess: 0.9,
-//   // transparent: true,
-//   // opacity: 0.7,
-// });
-const shinyMaterial = new THREE.MeshStandardMaterial({
-  map: texture,
-});
-const shinyMaterial2 = new THREE.MeshStandardMaterial({
-  map: texture4,
-});
-const cube = new THREE.Mesh(crystalGeometry, shinyMaterial);
-const cube2  =  new THREE.Mesh(crystalGeometry2, shinyMaterial2);
- 
-cube2.position.set(20 , -50 , 10)
+//----BACKGROUND
+// const texture2 = new THREE.TextureLoader().load({color:0x00ff00});
+// scene.background=texture2;
 
-cube.position.set(30, 5, -60);
-scene.add(cube, cube2);
+// CUBE
+const geometry = new THREE.BoxGeometry(15,25,15);
+const texture4 = new THREE.TextureLoader().load("./333.png");
+const material = new THREE.MeshStandardMaterial({  wireframe : false , map: texture4});
+const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
+scene.add(cube);
+cube.position.z = -30;
+cube.rotation.y = -12;
+cube.position.x = 22;
+cube.position.y = 12.5;
 
-const light = new THREE.PointLight(0xffffff);
-light.position.set(15, 15, 15);
+//CUBE2
+const geometry2 = new THREE.BoxGeometry(15,25,15);
+const texture42 = new THREE.TextureLoader().load("./logo2.png");
+const material2 = new THREE.MeshStandardMaterial({  wireframe : false , map: texture42});
+const cube2 = new THREE.Mesh(geometry2, material2);
+cube2.castShadow = true;
+scene.add(cube2);
+cube2.position.z = -60;
+cube2.rotation.y = 7;
+cube2.position.x = -22;
+cube2.position.y = 12.5;
+
+
+
+//SPEHERE
+const sphereGeometry = new THREE.SphereGeometry( 2, 100, 100);
+const texture5 = new THREE.TextureLoader().load("./earth.jpg");
+
+const sphereMaterial = new THREE.MeshStandardMaterial( { map : texture5 } );
+
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphere.castShadow = true; //default is false
+sphere.receiveShadow = false; //default
+scene.add( sphere );
+sphere.position.y = 10;
+sphere.position.z = -50;
+
+//PLANE 
+const planeGeometry = new THREE.PlaneGeometry( 100, 100, 32, 32 );
+const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff, side: THREE.DoubleSide} )
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.receiveShadow = true;
+plane.rotation.x = -Math.PI / 2;
+plane.position.z = -50;
+scene.add( plane );
+
+//road for camera
+const planeGeometry2 = new THREE.PlaneGeometry( 20, 100, 32, 32 );
+const planeMaterial2 = new THREE.MeshStandardMaterial( { color: 0x000000, side: THREE.DoubleSide} )
+const plane2 = new THREE.Mesh( planeGeometry2, planeMaterial2 );
+plane2.receiveShadow = true;
+plane2.rotation.x = -Math.PI / 2;
+plane2.position.z = -50;
+scene.add( plane2 );
+
+
+const light = new THREE.PointLight(0xffffff , 2 ,100);
+light.position.set(15, 50,-10 );
+light.castShadow = true; 
 scene.add(light);
 
-const ambloght = new THREE.AmbientLight(0xffffff);
-scene.add(ambloght);
 
-const helper3 = new THREE.PointLightHelper(light);
+const light2 = new THREE.PointLight(0xffffff, 2 ,100);
+light2.position.set(-15,25,-55);
+// light2.castShadow = true; 
+scene.add(light2);
+// light.shadow.mapSize.width = 512; // default
+// light.shadow.mapSize.height = 512; // default
+// light.shadow.camera.near = 0.5; // default
+// light.shadow.camera.far = 500; // default
+
+const helper = new THREE.CameraHelper( light.shadow.camera );
+const helper2 = new THREE.CameraHelper( light2.shadow.camera );
+scene.add( helper );
+scene.add(  helper2 );
+
 const gridhelp = new THREE.GridHelper(200, 50);
 // scene.add( gridhelp  );
 
-const control = new OrbitControls(camera, renderer.domElement);
+// const phelper = new THREE.PlaneHelper( plane, 1 , 0xffffff );
+// scene.add( phelper );
+const ambloght = new THREE.AmbientLight(0xffffff);
+scene.add(ambloght);
+// ambloght.castShadow = true; 
 
-function addstar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  // const texture = new THREE.TextureLoader().load();
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(200));
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-  star.position.set(x, y, z);
-  scene.add(star);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-Array(400).fill().forEach(addstar);
 
-// const texture2 = new THREE.TextureLoader().load(logo);
-// scene.background=texture2;
+// SCROLL MOVING CAMERA
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
 
-// function moveCamera() {
-//   const t = document.body.getBoundingClientRect().top;
+  camera.position.z = t * 0.01;
+  // camera.position.x = t * -0.002;
+  // camera.rotation.y = t * -0.002;
+}
+document.body.onscroll = moveCamera;
+moveCamera();
 
-//   camera.position.z = t * -0.01;
-//   camera.position.x = t * -0.002;
-//   camera.rotation.y = t * -0.002;
-// }
+// this.tl =  new TimeLineMax().delay(.3);
+// this.tl =  new TimeLineMax({paused : true});
+// this.tl.to(this.camera.position ,1, { z:-20 , ease :Expo.easeout})
 
-// document.body.onscroll = moveCamera;
-// moveCamera();
+function cameraZoomIn(){
+camera.position.z = -20;
+}
+
+document.getElementById('click').onclick = cameraZoomIn;
+
+// const control = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
-  cube2.rotation.x += 0.01;
-  // cube2.rotation.y += 0.01;
-  control.update();
+  sphere.rotation.x += 0.005;
+  sphere.rotation.y += 0.001;
   renderer.render(scene, camera);
+  window.addEventListener("resize", onWindowResize);
+  // control.update();
 }
-
 animate();
