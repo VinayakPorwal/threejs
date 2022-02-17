586a6e8e8bd7efafbc127f729dc024831740d968
fookrey420 <84093231+fookrey420@users.noreply.github.com>
2022-02-17 16:10:59 +0530

    f

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from "https://unpkg.com/three@0.120.1/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.120.1/examples/jsm/controls/OrbitControls.js";
 // import {TimeLineMax} from "https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js";
-
+// import TWEEN from "https://cdnjs.cloudflare.com/ajax/libs/tween.js/16.3.5/Tween.min.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -17,12 +17,14 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffff, 0.5);
+renderer.setClearColor(0x00daff);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-// camera.position.setZ(40);
+camera.position.z= 110;
 camera.position.setY(10);
 
+const camhelper = new THREE.CameraHelper( camera );
+scene.add( camhelper );
 
 //----BACKGROUND
 // const texture2 = new THREE.TextureLoader().load({color:0x00ff00});
@@ -53,6 +55,23 @@ cube2.position.x = -22;
 cube2.position.y = 12.5;
 
 
+//CUBE3
+const cube3 = cube2.clone();
+scene.add(cube3);
+cube3.position.z = -120;
+cube3.rotation.y = 7;
+cube3.position.x = -22;
+cube3.position.y = 12.5;
+
+
+//CUBE4
+const cube4 = cube.clone();
+scene.add(cube4);
+cube.position.z = -90;
+cube.rotation.y = -12;
+cube.position.x = 22;
+cube.position.y = 12.5;
+
 
 //SPEHERE
 const sphereGeometry = new THREE.SphereGeometry( 2, 100, 100);
@@ -68,52 +87,53 @@ sphere.position.y = 10;
 sphere.position.z = -50;
 
 //PLANE 
-const planeGeometry = new THREE.PlaneGeometry( 100, 100, 32, 32 );
-const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff, side: THREE.DoubleSide} )
+const planeGeometry = new THREE.PlaneGeometry( 300, 300, 32, 32 );
+const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffa500, side: THREE.DoubleSide} )
 const plane = new THREE.Mesh( planeGeometry, planeMaterial );
 plane.receiveShadow = true;
 plane.rotation.x = -Math.PI / 2;
-plane.position.z = -50;
+plane.position.z = -150;
 scene.add( plane );
 
 //road for camera
-const planeGeometry2 = new THREE.PlaneGeometry( 20, 100, 32, 32 );
+const planeGeometry2 = new THREE.PlaneGeometry( 20, 300, 32, 32 );
 const planeMaterial2 = new THREE.MeshStandardMaterial( { color: 0x000000, side: THREE.DoubleSide} )
 const plane2 = new THREE.Mesh( planeGeometry2, planeMaterial2 );
 plane2.receiveShadow = true;
 plane2.rotation.x = -Math.PI / 2;
-plane2.position.z = -50;
+plane2.position.z = -150;
+plane2.position.y= 0.1;
 scene.add( plane2 );
 
 
-const light = new THREE.PointLight(0xffffff , 2 ,100);
-light.position.set(15, 50,-10 );
+//LIGHT
+const light = new THREE.PointLight(0xffffff , 1,100);
+light.position.set(15, 150,-10 );
 light.castShadow = true; 
 scene.add(light);
-
-
-const light2 = new THREE.PointLight(0xffffff, 2 ,100);
-light2.position.set(-15,25,-55);
+//----
+const light2 = new THREE.PointLight(0xffffff, 1 ,100);
+light2.position.set(-15,155,-55);
 // light2.castShadow = true; 
 scene.add(light2);
-// light.shadow.mapSize.width = 512; // default
-// light.shadow.mapSize.height = 512; // default
-// light.shadow.camera.near = 0.5; // default
-// light.shadow.camera.far = 500; // default
+//----
+const ambloght = new THREE.AmbientLight(0xffffff);
+scene.add(ambloght);
+// ambloght.castShadow = true; 
 
+//LIGHT HELPER
 const helper = new THREE.CameraHelper( light.shadow.camera );
 const helper2 = new THREE.CameraHelper( light2.shadow.camera );
-scene.add( helper );
-scene.add(  helper2 );
+// scene.add( helper );
+// scene.add(  helper2 );
+
+
 
 const gridhelp = new THREE.GridHelper(200, 50);
 // scene.add( gridhelp  );
 
 // const phelper = new THREE.PlaneHelper( plane, 1 , 0xffffff );
 // scene.add( phelper );
-const ambloght = new THREE.AmbientLight(0xffffff);
-scene.add(ambloght);
-// ambloght.castShadow = true; 
 
 
 function onWindowResize() {
@@ -128,7 +148,7 @@ function onWindowResize() {
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
 
-  camera.position.z = t * 0.01;
+  camera.position.z = t * 0.02;
   // camera.position.x = t * -0.002;
   // camera.rotation.y = t * -0.002;
 }
@@ -140,10 +160,15 @@ moveCamera();
 // this.tl.to(this.camera.position ,1, { z:-20 , ease :Expo.easeout})
 
 function cameraZoomIn(){
-camera.position.z = -20;
+// camera.position.z = -20;
+requestAnimationFrame(cameraZoomIn);
+sphere.position.y = 20;
+renderer.render(scene, camera);
+
 }
 
-document.getElementById('click').onclick = cameraZoomIn;
+// document.getElementById('click').onclick = cameraZoomIn;
+
 
 // const control = new OrbitControls(camera, renderer.domElement);
 
